ebda22baf13829997248fa4e7e55743d88ea196e
J. Hernan Ramirez R <hr@hernanramirez.info>
2018-04-19 07:19:00 -0400

    Integración flakes8

--- divulgacion/divulgacion_app/urls.py
+++ divulgacion/divulgacion_app/urls.py
@@ -1,5 +1,4 @@
 from django.conf.urls import url
-
 from divulgacion.divulgacion_app.views import noticias
 from divulgacion.divulgacion_app.views import api
 
@@ -14,35 +13,29 @@ urlpatterns = [
         view=noticias.NoticiaCreateView.as_view(),
         name='noticia_create'
     ),
-
     url(
         regex=r'^noticia/view/(?P<pk>\d+)$',
         view=noticias.NoticiaDetailView.as_view(),
         name='noticia_view'
     ),
-
     url(
         regex=r'^noticia/update/(?P<pk>\d+)$',
         view=noticias.NoticiaUpdateView.as_view(),
         name='noticia_update'
     ),
-
     url(
         regex=r'^noticia/delete/(?P<pk>\d+)$',
         view=noticias.NoticiaDeleteView.as_view(),
         name='noticia_delete'
     ),
-
     url(
         regex=r'^api/noticias/$',
         view=api.NoticiasRestList.as_view(),
         name='noticia_rest_list'
     ),
-
     url(
         regex=r'^api/noticias/(?P<pk>\d+)$',
         view=api.NoticiaRestDetail.as_view(),
         name='noticia_rest_list'
     ),
-
 ]
