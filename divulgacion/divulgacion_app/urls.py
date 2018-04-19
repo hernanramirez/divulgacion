6aeaea1636cec608b85439f8024dc6ba753ffdc3
J. Hernan Ramirez R <hr@hernanramirez.info>
2018-04-19 07:26:03 -0400

    Integración flakes8

--- divulgacion/divulgacion_app/urls.py
+++ divulgacion/divulgacion_app/urls.py
@@ -1,41 +1,46 @@
 from django.conf.urls import url
-from divulgacion.divulgacion_app.views import noticias
-from divulgacion.divulgacion_app.views import api
+from divulgacion.divulgacion_app.views.noticias import (
+    NoticiaListView, NoticiaCreateView, NoticiaDetailView,
+    NoticiaUpdateView, NoticiaDeleteView
+)
+from divulgacion.divulgacion_app.views.api import (
+    NoticiasRestList, NoticiaRestDetail
+)
 
 urlpatterns = [
     url(
         regex=r'^noticia/$',
-        view=noticias.NoticiaListView.as_view(),
+        view=NoticiaListView.as_view(),
         name='noticia_list'
     ),
     url(
         regex=r'^noticia/create/$',
-        view=noticias.NoticiaCreateView.as_view(),
+        view=NoticiaCreateView.as_view(),
         name='noticia_create'
     ),
     url(
         regex=r'^noticia/view/(?P<pk>\d+)$',
-        view=noticias.NoticiaDetailView.as_view(),
+        view=NoticiaDetailView.as_view(),
         name='noticia_view'
     ),
     url(
         regex=r'^noticia/update/(?P<pk>\d+)$',
-        view=noticias.NoticiaUpdateView.as_view(),
+        view=NoticiaUpdateView.as_view(),
         name='noticia_update'
     ),
     url(
         regex=r'^noticia/delete/(?P<pk>\d+)$',
-        view=noticias.NoticiaDeleteView.as_view(),
+        view=NoticiaDeleteView.as_view(),
         name='noticia_delete'
     ),
     url(
         regex=r'^api/noticias/$',
-        view=api.NoticiasRestList.as_view(),
+        view=NoticiasRestList.as_view(),
         name='noticia_rest_list'
     ),
     url(
         regex=r'^api/noticias/(?P<pk>\d+)$',
-        view=api.NoticiaRestDetail.as_view(),
+        view=NoticiaRestDetail.as_view(),
         name='noticia_rest_list'
     ),
 ]
